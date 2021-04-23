6d6550f3bf4e20394023d7dc323f665e8e8791d8
djibsonx7 <djibril7diakhate@gmail.com>
2021-04-23 02:42:46 +0000

    Test

--- GestionDeCampagneBack/Models/dbGestionDeCampagneContext.cs
+++ GestionDeCampagneBack/Models/dbGestionDeCampagneContext.cs
@@ -52,14 +52,14 @@ namespace GestionDeCampagneBack.Models
         public virtual DbSet<Utilisateur> Utilisateurs { get; set; }
         public virtual DbSet<Variable> Variables { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-LFQAKSS;Database=dbGestionDeCampagne;Trusted_Connection=True;");
-            }
-        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+          //  if (!optionsBuilder.IsConfigured)
+            //{
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+             //   optionsBuilder.UseSqlServer("Server=DESKTOP-LFQAKSS;Database=dbGestionDeCampagne;Trusted_Connection=True;");
+  //          }
+    //    }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
