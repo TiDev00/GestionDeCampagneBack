32573df9dafac23bdadf844a4d0efd21c73e2c01
djibsonx7 <djibril7diakhate@gmail.com>
2021-04-25 22:07:44 +0000

    Gestion contact canal

---
+++ GestionDeCampagneBack/Repository/IContactCanal.cs
@@ -0,0 +1,22 @@
+﻿using GestionDeCampagneBack.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestionDeCampagneBack.Repository
+{
+    public interface IContactCanal
+    {
+        bool SaveChanges();
+        List<ContactCanal> GetContactCanals();
+
+        ContactCanal GetContactCanalById(int id);
+
+        void AddContactCanal(ContactCanal ContactCanal);
+
+        void DeleteContactCanal(ContactCanal ContactCanal);
+
+        ContactCanal EditContactCanal(ContactCanal ContactCanal, int id);
+    }
+}
